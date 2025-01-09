8dace08d08c78330ec8763bf09f10c2374a2921b
Roman Soltan <romabandit88@gmail.com>
2025-01-09 07:03:57 +0200

    settings props

--- module_1/lesson_1/src/components/Product.jsx
+++ module_1/lesson_1/src/components/Product.jsx
@@ -1,12 +1,12 @@
-export default function Product() {
-  const price = 999;
-  const imgUrl =
-    'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640';
-
+export default function Product({
+  name,
+  imgUrl = 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640',
+  price,
+}) {
   return (
     <div>
-      <h2>Tacos</h2>
-      <img src={imgUrl} alt="Tacos With Lime" width="640" />
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
       <p>Price: {price} credits</p>
     </div>
   );
