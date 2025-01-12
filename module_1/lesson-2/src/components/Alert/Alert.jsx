af6e678b3eea49c3dafeb6794d2e7f9bac7f2d8a
Roman Soltan <romabandit88@gmail.com>
2025-01-12 18:11:38 +0200

    clsx

--- module_1/lesson-2/src/components/Alert/Alert.jsx
+++ module_1/lesson-2/src/components/Alert/Alert.jsx
@@ -1,17 +1,17 @@
+import clsx from 'clsx';
 import './Alert.css';
 
 const Alert = ({ variant, outlined, elevated, children }) => {
-  const classNames = ['alert', variant];
-
-  if (outlined) {
-    classNames.push('is-outlined');
-  }
-
-  if (elevated) {
-    classNames.push('is-elevated');
-  }
-
-  return <p className={classNames.join(' ')}>{children}</p>;
+  return (
+    <p
+      className={clsx('alert', variant, {
+        'is-outlined': outlined,
+        'is-elevated': elevated,
+      })}
+    >
+      {children}
+    </p>
+  );
 };
 
 export default Alert;
