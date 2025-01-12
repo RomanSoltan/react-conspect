5eb944c633e3e2929be27920a96e800b01fa0497
Roman Soltan <romabandit88@gmail.com>
2025-01-12 18:04:34 +0200

    compos2

--- module_1/lesson-2/src/components/Alert/Alert.jsx
+++ module_1/lesson-2/src/components/Alert/Alert.jsx
@@ -1,7 +1,16 @@
 import './Alert.css';
 
-const Alert = ({ variant, children }) => {
+const Alert = ({ variant, outlined, elevated, children }) => {
   const classNames = ['alert', variant];
+
+  if (outlined) {
+    classNames.push('is-outlined');
+  }
+
+  if (elevated) {
+    classNames.push('is-elevated');
+  }
+
   return <p className={classNames.join(' ')}>{children}</p>;
 };
 
