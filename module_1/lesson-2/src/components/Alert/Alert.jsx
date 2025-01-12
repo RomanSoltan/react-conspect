1d230d238d97b67656a4f0c554e0a5149cb7606d
Roman Soltan <romabandit88@gmail.com>
2025-01-12 18:00:56 +0200

    compos 1

--- module_1/lesson-2/src/components/Alert/Alert.jsx
+++ module_1/lesson-2/src/components/Alert/Alert.jsx
@@ -1,37 +1,8 @@
-const alertStyles = {
-  margin: 8,
-  padding: '12px 16px',
-  borderRadius: 4,
-  backgroundColor: 'gray',
-  color: 'white',
-};
-
-const getBgColor = variant => {
-  switch (variant) {
-    case 'info':
-      return 'blue';
-    case 'error':
-      return 'red';
-    case 'success':
-      return 'green';
-    case 'warning':
-      return 'orange';
-    default:
-      throw new Error(`Unsupported variant prop value - ${variant}`);
-  }
-};
+import './Alert.css';
 
 const Alert = ({ variant, children }) => {
-  return (
-    <p
-      style={{
-        ...alertStyles,
-        backgroundColor: getBgColor(variant),
-      }}
-    >
-      {children}
-    </p>
-  );
+  const classNames = ['alert', variant];
+  return <p className={classNames.join(' ')}>{children}</p>;
 };
 
 export default Alert;
