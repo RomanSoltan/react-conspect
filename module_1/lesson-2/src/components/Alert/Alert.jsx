d9343275e0e1ad9368b2fbd3e1cd662a840dc4e6
Roman Soltan <romabandit88@gmail.com>
2025-01-12 18:27:30 +0200

    module.css

--- module_1/lesson-2/src/components/Alert/Alert.jsx
+++ module_1/lesson-2/src/components/Alert/Alert.jsx
@@ -1,13 +1,14 @@
 import clsx from 'clsx';
-import './Alert.css';
+import css from './Alert.module.css';
 
 const Alert = ({ variant, outlined, elevated, children }) => {
   return (
     <p
-      className={clsx('alert', variant, {
-        'is-outlined': outlined,
-        'is-elevated': elevated,
-      })}
+      className={clsx(
+        css[variant],
+        outlined && css.isOutlined,
+        elevated && css.isElevated,
+      )}
     >
       {children}
     </p>
