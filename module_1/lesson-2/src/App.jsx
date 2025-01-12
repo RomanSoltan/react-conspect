bfdabdcb7615afeae2d9d0bdb784ae0d9c1512d5
Roman Soltan <romabandit88@gmail.com>
2025-01-12 19:58:49 +0200

    reuse styles

--- module_1/lesson-2/src/App.jsx
+++ module_1/lesson-2/src/App.jsx
@@ -1,11 +1,17 @@
 import './components/Alert/Alert';
 import Alert from './components/Alert/Alert';
 import UserMenu from './components/UserMenu/UserMenu';
+import FollowButton from './components/FollowButon/FollowButon';
+import LoginButton from './components/LoginButton/LoginButton';
 
 const App = () => {
   return (
     <>
       <UserMenu name="Roman" />
+
+      <FollowButton />
+      <LoginButton />
+
       <Alert variant="info">Please update your email!</Alert>
       <Alert variant="error">There was an error during transaction!</Alert>
       <Alert variant="success">
---
+++ module_1/lesson-2/src/components/LoginButton/LoginButton.jsx
@@ -0,0 +1,8 @@
+import Button from '../Button/Button';
+
+const LoginButton = () => {
+  // Унікальна логіка кнопки логіна
+  return <Button variant="primary">Login</Button>;
+};
+
+export default LoginButton;
