6a33bd7b2c03fde86f98dd8d567c60440f2b0dc8
Roman Soltan <romabandit88@gmail.com>
2025-01-13 12:08:01 +0200

    obj event

--- module_2/lesson-3/src/App.jsx
+++ module_2/lesson-3/src/App.jsx
@@ -1,14 +1,29 @@
 import './App.css';
 
 const App = () => {
-  const handleClick = () => {
-    alert('I am a button');
+  // =======================
+  //Об'єкт події
+
+  const handleClick = evt => {
+    console.log(evt);
   };
 
-  return <button onClick={handleClick}>Click me!</button>;
+  return (
+    <>
+      <button onClick={handleClick}>First button</button>
+      <button onClick={evt => console.log(evt)}>Second button</button>
+    </>
+  );
 
-  // інший спосіб
-  // return <button onClick={() => alert("I'm a button!")}>Click me!</button>;
+  // =============================
+  // Обробка подій
+  // const handleClick = () => {
+  //   alert('I am a button');
+  // };
+  // return <button onClick={handleClick}>Click me!</button>;
+
+  // // інший спосіб
+  // // return <button onClick={() => alert("I'm a button!")}>Click me!</button>;
 };
 
 export default App;
