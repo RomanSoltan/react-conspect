30beaa05569f388444775241667885b61aa7507e
Roman Soltan <romabandit88@gmail.com>
2025-01-13 16:39:06 +0200

    update obj

--- module_2/lesson-3/src/App.jsx
+++ module_2/lesson-3/src/App.jsx
@@ -3,28 +3,57 @@ import { useState } from 'react';
 // import ClickCounter from './components/ClickCounter/ClickCounter';
 import './App.css';
 
-const ClickCounter = ({ value, onUpdate }) => {
-  return (
-    <button onClick={onUpdate}>
-      Current:
-      {value}
-    </button>
-  );
-};
+// const ClickCounter = ({ value, onUpdate }) => {
+//   return (
+//     <button onClick={onUpdate}>
+//       Current:
+//       {value}
+//     </button>
+//   );
+// };
 
 const App = () => {
-  // Підняття стану
-  const [clicks, setClicks] = useState(0);
-  const handleClick = () => {
-    setClicks(clicks + 1);
+  //Оновлення об'єктів
+  const [values, setValues] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const updateX = () => {
+    setValues({
+      ...values,
+      x: values.x + 1,
+    });
+  };
+
+  const updateY = () => {
+    setValues({
+      ...values,
+      y: values.y + 1,
+    });
   };
 
   return (
-    <>
-      <ClickCounter value={clicks} onUpdate={handleClick} />
-      <ClickCounter value={clicks} onUpdate={handleClick} />
-    </>
+    <div>
+      <p>
+        x: {values.x}, y: {values.y}
+      </p>
+      <button onClick={updateX}>Update x</button>
+      <button onClick={updateY}>Update y</button>
+    </div>
   );
+  // ==============================================
+  // // Підняття стану
+  // const [clicks, setClicks] = useState(0);
+  // const handleClick = () => {
+  //   setClicks(clicks + 1);
+  // };
+  // return (
+  //   <>
+  //     <ClickCounter value={clicks} onUpdate={handleClick} />
+  //     <ClickCounter value={clicks} onUpdate={handleClick} />
+  //   </>
+  // );
   // =======================================
   // //Ізоляція стану
   // return (
