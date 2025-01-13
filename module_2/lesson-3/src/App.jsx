ff406eece0ee9e57ebb090cffa4f4f81d6abeaa1
Roman Soltan <romabandit88@gmail.com>
2025-01-13 16:29:05 +0200

    pidnyatya stanu

--- module_2/lesson-3/src/App.jsx
+++ module_2/lesson-3/src/App.jsx
@@ -1,17 +1,39 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 // import CustomButton from './components/CustomButton/CustomButton';
-import ClickCounter from './components/ClickCounter/ClickCounter';
+// import ClickCounter from './components/ClickCounter/ClickCounter';
 import './App.css';
 
+const ClickCounter = ({ value, onUpdate }) => {
+  return (
+    <button onClick={onUpdate}>
+      Current:
+      {value}
+    </button>
+  );
+};
+
 const App = () => {
-  //Ізоляція стану
+  // Підняття стану
+  const [clicks, setClicks] = useState(0);
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
   return (
     <>
-      <ClickCounter />
-      <ClickCounter />
+      <ClickCounter value={clicks} onUpdate={handleClick} />
+      <ClickCounter value={clicks} onUpdate={handleClick} />
     </>
   );
-  // ===========================
+  // =======================================
+  // //Ізоляція стану
+  // return (
+  //   <>
+  //     <ClickCounter />
+  //     <ClickCounter />
+  //   </>
+  // );
+  // =======================================
   // //Декілька станів
   // const [clicks, setClicks] = useState(0);
   // const [isOpen, setIsOpen] = useState(false);
@@ -28,7 +50,7 @@ const App = () => {
   //     {isOpen && <p>Now you can see me!</p>}
   //   </>
   // );
-  // ====================
+  // ========================================
   // // Реактивність
   // // let clicks = 0;
   // const [clicks, setClicks] = useState(0);
@@ -37,7 +59,7 @@ const App = () => {
   //   setClicks(clicks + 1);
   // };
   // return <button onClick={handleClick}>Current: {clicks}</button>;
-  // =======================
+  // ===============================================
   // // Об'єкт події
   // const handleClick = evt => {
   //   console.log(evt);
@@ -48,7 +70,7 @@ const App = () => {
   //     <button onClick={evt => console.log(evt)}>Second button</button>
   //   </>
   // );
-  // ====================
+  // ====================================================
   // Обробка подій
   // const handleClick = () => {
   //   alert('I am a button');
