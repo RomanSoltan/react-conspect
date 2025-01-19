12b9422dd31d49d69bed1d47624da9fa6d5a1cd9
Roman Soltan <romabandit88@gmail.com>
2025-01-19 22:30:30 +0200

    init repo

---
+++ module_3/lesson-5/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p>hello</p>
+    </div>
+  );
+}
+
+export default App;
