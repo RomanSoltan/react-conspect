4396c500a4c026e9cb18843ee7fcb4898eee1743
Roman Soltan <romabandit88@gmail.com>
2025-01-19 23:35:59 +0200

    huck useId

--- module_3/lesson-5/src/App.jsx
+++ module_3/lesson-5/src/App.jsx
@@ -2,6 +2,7 @@
 
 import './App.css';
 import LoginForm from './components/LoginForm/LoginForm';
+import MyComponent from './components/MyComponent/MyComponent';
 
 function App() {
   // Колбек-функція для обробки сабміту форми
@@ -15,6 +16,7 @@ function App() {
       <h1>Please login to your account!</h1>
       {/* Передаємо колбек як пропс форми */}
       <LoginForm onLogin={handleLogin} />
+      <MyComponent />
     </div>
   );
 }
