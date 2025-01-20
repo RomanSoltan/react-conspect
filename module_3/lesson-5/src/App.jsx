b7b5421853fdead79455acb864785f9acebe4fc9
Roman Soltan <romabandit88@gmail.com>
2025-01-20 18:31:00 +0200

    select

--- module_3/lesson-5/src/App.jsx
+++ module_3/lesson-5/src/App.jsx
@@ -1,11 +1,13 @@
-// import { useState } from 'react'
+import { useState } from 'react';
 
 import './App.css';
+import LangSwitcher from './components/LangSwitcher/LangSwitcher';
 import LoginForm from './components/LoginForm/LoginForm';
 import MyComponent from './components/MyComponent/MyComponent';
 import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
+  const [lang, setLang] = useState('uk');
   // Колбек-функція для обробки сабміту форми
   const handleLogin = userData => {
     // Виконуємо необхідні операції з даними
@@ -14,6 +16,8 @@ function App() {
 
   return (
     <div>
+      <p>Selected language: {lang}</p>
+      <LangSwitcher value={lang} onSelect={setLang} />
       <SearchBar />
       <h1>Please login to your account!</h1>
       {/* Передаємо колбек як пропс форми */}
