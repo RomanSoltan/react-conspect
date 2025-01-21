ff68a1981aba354ce11ea86803a8db497cbf6c33
Roman Soltan <romabandit88@gmail.com>
2025-01-21 22:10:09 +0200

    add

--- module_3/workshop/src/components/App/App.jsx
+++ module_3/workshop/src/components/App/App.jsx
@@ -3,15 +3,34 @@ import initialTasks from "../../tasks.json";
 import Form from "../Form/Form";
 import Filter from "../Filter/Filter";
 import css from "./App.module.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+  const [filter, setFilter] = useState("");
+
+  const addTask = (newTask) => {
+    setTasks((prev) => {
+      return [...prev, newTask];
+    });
+  };
+
+  const deleteTask = (taskId) => {
+    console.log(taskId);
+    setTasks((prev) => {
+      return prev.filter((item) => item.id !== taskId);
+    });
+  };
+
+  const visibleTasks = tasks.filter((task) =>
+    task.text.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div className={css.container}>
-      <Form />
-      <Filter />
-      <TaskList />
+      <Form onAdd={addTask} />
+      <Filter value={filter} onFilter={setFilter} />
+      <TaskList tasks={visibleTasks} onDelete={deleteTask} />
     </div>
   );
 }
-
-export default App;
