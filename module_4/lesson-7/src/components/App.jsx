b9acf3bd25780f022cbcf2983b9150d57be04fff
Roman Soltan <romabandit88@gmail.com>
2025-01-27 16:53:20 +0200

    add leson 7

--- module_4/lesson-7/src/components/App.jsx
+++ module_4/lesson-7/src/components/App.jsx
@@ -1,9 +1,63 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import ArticleList from "./ArticleList/ArticleList";
+import { fetchArticlesWithTopic } from "../articles-api";
+import SearchForm from "./SearchForm/SearchForm";
+import Loader from "./Loader/Loader";
+import Error from "./Error/Error";
 
 function App() {
+  // 3. Оголошуємо стан
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  // useEffect(() => {
+  //   // 1. Оголошуємо асинхронну функцію
+  //   async function fetchArticles() {
+  //     try {
+  //       // 5. Встановлюємо індикатор в true перед запитом
+  //       setLoading(true);
+  //       // Тут будемо виконувати HTTP-запит
+  //       // Використовуємо HTTP-функцію
+  //       const data = await fetchArticlesWithTopic("react");
+  //       // 4. Записуємо дані в стан
+  //       setArticles(data);
+  //     } catch (error) {
+  //       // Тут будемо обробляти помилку
+  //       // Встановлюємо стан error в true
+  //       setError(true);
+  //     } finally {
+  //       // 6. Встановлюємо індикатор в false після запиту
+  //       setLoading(false);
+  //     }
+  //   }
+  //   // 2. Викликаємо її одразу після оголошення
+  //   fetchArticles();
+  // }, []);
+
+  const handleSearch = async (topic) => {
+    try {
+      setArticles([]);
+      setError(false);
+      setLoading(true);
+      const data = await fetchArticlesWithTopic(topic);
+      setArticles(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
-      <p>lesson 7</p>
+      <SearchForm onSearch={handleSearch} />
+      {loading && <Loader />}
+      {/* Помилка */}
+      {error && <Error />}
+      {/* 5. Використовувати стан для відображення JSX-розмітки */}
+      {articles.length > 0 && <ArticleList items={articles} />}
     </div>
   );
 }
---
+++ module_4/lesson-7/src/components/Loader/Loader.jsx
@@ -0,0 +1,4 @@
+const Loader = () => {
+  return <p style={{ fontSize: 20 }}>Loading data, please wait...</p>;
+};
+export default Loader;
