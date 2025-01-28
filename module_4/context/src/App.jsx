d5c0a1815e2f0a9a817a313b816e809a1fe17381
Roman Soltan <romabandit88@gmail.com>
2025-01-28 22:34:45 +0200

    context

---
+++ module_4/context/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UserMenu from "./components/UserMenu/UserMenu";
+
+function App() {
+  return (
+    <div>
+      <UserMenu />
+    </div>
+  );
+}
+
+export default App;
