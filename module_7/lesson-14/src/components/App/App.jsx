78982f72f72adffd2b83a5b52650b40f4bc8266d
Roman Soltan <romabandit88@gmail.com>
2025-02-18 19:50:39 +0200

    selectors

--- module_7/lesson-14/src/components/App/App.jsx
+++ module_7/lesson-14/src/components/App/App.jsx
@@ -1,16 +1,19 @@
+import { useEffect } from "react";
+// Імпорти компонентів
+import { useDispatch, useSelector } from "react-redux";
 import AppBar from "../AppBar/AppBar";
 import TaskForm from "../TaskForm/TaskForm";
 import TaskList from "../TaskList/TaskList";
-import s from "./App.module.css";
-// Імпорти компонентів
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 import { fetchTasks } from "../../redux/operations";
+// 1. Імпортуємо функції-селектори
+import { selectError, selectIsLoading } from "../../redux/selectors";
+import s from "./App.module.css";
 
 function App() {
   const dispatch = useDispatch();
-  const isLoading = useSelector((state) => state.tasks.isLoading);
-  const error = useSelector((state) => state.tasks.error);
+  // 2. Використуваєм їх для отримання значень стану
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchTasks());
--- module_7/lesson-14/src/components/TaskList/TaskList.jsx
+++ module_7/lesson-14/src/components/TaskList/TaskList.jsx
@@ -1,26 +1,14 @@
 import { useSelector } from "react-redux";
 import s from "./TaskList.module.css";
 import Task from "../Task/Task";
-
-const getVisibleTasks = (tasks, statusFilter) => {
-  switch (statusFilter) {
-    case "active":
-      return tasks.filter((task) => !task.completed);
-    case "completed":
-      return tasks.filter((task) => task.completed);
-    default:
-      return tasks;
-  }
-};
+import { selectVisibleTasks } from "../../redux/selectors";
 
 const TaskList = () => {
-  const tasks = useSelector((state) => state.tasks.items);
-  const statusFilter = useSelector((state) => state.filters.status);
-  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+  const tasks = useSelector(selectVisibleTasks);
 
   return (
     <ul className={s.list}>
-      {visibleTasks.map((task) => (
+      {tasks.map((task) => (
         <li className={s.listItem} key={task.id}>
           <Task task={task} />
         </li>
