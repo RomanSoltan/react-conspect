b54b8735a85b35bbd3523d20282c43f3553b8b1c
Roman Soltan <romabandit88@gmail.com>
2025-02-23 18:31:32 +0200

    add main

--- module_8/lesson-15_user-registration-login/vite-react-starter/src/main.jsx
+++ module_8/lesson-15_user-registration-login/vite-react-starter/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { HelmetProvider } from "react-helmet-async";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import App from "./components/App.jsx";
+import { persistor, store } from "./redux/store.js";
+import "modern-normalize";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <HelmetProvider>
+            <App />
+          </HelmetProvider>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
